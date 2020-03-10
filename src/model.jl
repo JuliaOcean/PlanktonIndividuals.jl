b3fd40a1475f1d121998815693f6a676c0bfa6e8
zhenwu0728 <zhenwu0728@users.noreply.github.com>
2020-03-10 11:57:44 -0400

    code clean up

--- src/model.jl
+++ src/model.jl
@@ -20,76 +20,7 @@ function PA_Model(grid, RunParam;
     end
     return model
 end
-"""
-    PA_ModelRun(model::Model_struct, Rumparam, RunOption)
-The function to run the model for number of time steps in RunParam
-'model' is generated from 'PA_model'
-The function use 'PA_advect!', a simple advection for individuals
-"""
-function PA_ModelRun(model::Model_struct, RunParam::RunParams, RunOption::RunOptions)
-    if RunOption.NutOutputChoice == false
-        DIC = zeros(g.Nx, g.Ny, g.Nz, nTime)
-        NH4 = zeros(g.Nx, g.Ny, g.Nz, nTime)
-        NO3 = zeros(g.Nx, g.Ny, g.Nz, nTime)
-        PO4 = zeros(g.Nx, g.Ny, g.Nz, nTime)
-        DOC = zeros(g.Nx, g.Ny, g.Nz, nTime)
-        DON = zeros(g.Nx, g.Ny, g.Nz, nTime)
-        DOP = zeros(g.Nx, g.Ny, g.Nz, nTime)
-        POC = zeros(g.Nx, g.Ny, g.Nz, nTime)
-        PON = zeros(g.Nx, g.Ny, g.Nz, nTime)
-        POP = zeros(g.Nx, g.Ny, g.Nz, nTime)
-    end
-    if RunOption.VelChoice
-        store_vel=load(dirname(pathof(PhytoAgentModel))*"/../samples/uvw.jld", "uvw")
-    end
 
-    for it in 1:RunParam.nTime
-        t = model.t
-        phyts_a = copy(model.individuals[end]) # read data from last time step
-        phyts_b,counts,consume=phyt_update(t, RunParam.ΔT, phyts_a, model)
-        if RunOption.VelChoice == false
-            velᵇ = read_offline_vels(RunOption.VelOfflineOpt,trunc(Int,t*RunParam.ΔT/3600))
-        else
-            velᵇ=store_vel[t]
-        end
-        if (model.grid.Nx == 1) & (model.grid.Ny == 1) & (model.grid.Nz == 1)
-            nothing #for 0D only
-        else
-            vel_itp = generate_vel_itp(model.grid, velᵇ)
-            PA_advect!(model, RunParam.ΔT, vel_itp)
-        end
-        push!(model.individuals,phyts_b)
-        write_output(t,phyts_b,counts,model.output)
-        agent_num = size(phyts_b,1)
-        nutₜ,gtr = nut_update(model, velᵇ, consume, RunParam.ΔT)
-        if RunOption.OutputChoice
-            write_nut_cons(model.grid, gtr, nutₜ, velᵇ, agent_num, t, death_ct, graz_ct, dvid_ct)
-            if RunOption.NutOutputChoice
-                write_nut_nc_each_step(model.grid, nutₜ, t)
-            else
-                DIC[:,:,:,t] = nutₜ.DIC
-                NH4[:,:,:,t] = nutₜ.NH4
-                NO3[:,:,:,t] = nutₜ.NO3
-                PO4[:,:,:,t] = nutₜ.PO4
-                DOC[:,:,:,t] = nutₜ.DOC
-                DON[:,:,:,t] = nutₜ.DON
-                DOP[:,:,:,t] = nutₜ.DOP
-                POC[:,:,:,t] = nutₜ.POC
-                PON[:,:,:,t] = nutₜ.PON
-                POP[:,:,:,t] = nutₜ.POP
-            end
-        else
-            nothing
-        end
-        model.nutrients = nutₜ;
-        model.t += 1
-    end
-    if RunOption.NutOutputChoice == false
-        write_nut_nc_alltime(g, DIC, NH4, NO3, PO4, DOC, DON, DOP, POC, PON, POP, RunParam.nTime)
-    else
-        return nothing
-    end
-end
 """
     PA_advect!(individuals, ΔT, vel)
 Individual advection using a simple scheme
@@ -140,7 +71,7 @@ function PA_TimeStep!(model::Model_struct, ΔT, velᵇ::velocity, resultspath)
     model.individuals.phytos = phyts_b
     if model.individuals.zoos ≠ nothing
         zoos_b,counts_z,consume_z =zoo_update(model, ΔT)
-        consume_p = sum_consume(consume_p, consume_z)
+        consume_p = sum_nut_tendency(consume_p, consume_z)
         model.individuals.zoos = zoos_b
         counts_p.graze = counts_z.graze
     end
@@ -156,7 +87,7 @@ function PA_TimeStep!(model::Model_struct, ΔT, velᵇ::velocity)
     model.individuals.phytos = phyts_b
     if model.individuals.zoos ≠ nothing
         zoos_b,counts_z,consume_z =zoo_update(model, ΔT)
-        consume_p = sum_consume(consume_p, consume_z)
+        consume_p = sum_nut_tendency(consume_p, consume_z)
         model.individuals.zoos = zoos_b
         counts_p.graze = counts_z.graze
     end
