48a074dd3f9997729052f01a4d272145949ab050
zhenwu0728 <zhenwu@mit.edu>
2020-08-29 16:04:35 -0400

    include halo points in nutrient fields

--- src/model_setup.jl
+++ src/model_setup.jl
@@ -133,19 +133,21 @@ end
     nutrients_init(g)
 """
 function nutrients_init(::CPUs,g)
-    nut = nutrient_fields(zeros(g.Nx, g.Ny, g.Nz), zeros(g.Nx, g.Ny, g.Nz),
-                          zeros(g.Nx, g.Ny, g.Nz), zeros(g.Nx, g.Ny, g.Nz),
-                          zeros(g.Nx, g.Ny, g.Nz), zeros(g.Nx, g.Ny, g.Nz),
-                          zeros(g.Nx, g.Ny, g.Nz), zeros(g.Nx, g.Ny, g.Nz),
-                          zeros(g.Nx, g.Ny, g.Nz), zeros(g.Nx, g.Ny, g.Nz))
+    total_size = (g.Nx+g.Hx*2, g.Ny+g.Hy*2, g.Nz+g.Hz*2)
+    nut = nutrient_fields(zeros(total_size), zeros(total_size),
+                          zeros(total_size), zeros(total_size),
+                          zeros(total_size), zeros(total_size),
+                          zeros(total_size), zeros(total_size),
+                          zeros(total_size), zeros(total_size))
     return nut
 end
 function nutrients_init(::GPUs,g)
-    nut = nutrient_fields(CuArray(zeros(g.Nx, g.Ny, g.Nz)), CuArray(zeros(g.Nx, g.Ny, g.Nz)),
-                          CuArray(zeros(g.Nx, g.Ny, g.Nz)), CuArray(zeros(g.Nx, g.Ny, g.Nz)),
-                          CuArray(zeros(g.Nx, g.Ny, g.Nz)), CuArray(zeros(g.Nx, g.Ny, g.Nz)),
-                          CuArray(zeros(g.Nx, g.Ny, g.Nz)), CuArray(zeros(g.Nx, g.Ny, g.Nz)),
-                          CuArray(zeros(g.Nx, g.Ny, g.Nz)), CuArray(zeros(g.Nx, g.Ny, g.Nz)))
+    total_size = (g.Nx+g.Hx*2, g.Ny+g.Hy*2, g.Nz+g.Hz*2)
+    nut = nutrient_fields(CuArray(zeros(total_size)), CuArray(zeros(total_size)),
+                          CuArray(zeros(total_size)), CuArray(zeros(total_size)),
+                          CuArray(zeros(total_size)), CuArray(zeros(total_size)),
+                          CuArray(zeros(total_size)), CuArray(zeros(total_size)),
+                          CuArray(zeros(total_size)), CuArray(zeros(total_size)))
     return nut
 end
 
@@ -156,31 +158,33 @@ Set up initial nutrient fields according to grid information
 'nut' is an array of 10 elements, each element is a kind of nutrient
 """
 function setup_nutrients(::CPUs,g,nut)
-    DIC = fill(nut[1],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
-    NH4 = fill(nut[2],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
-    NO3 = fill(nut[3],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
-    PO4 = fill(nut[4],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
-    DOC = fill(nut[5],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
-    DON = fill(nut[6],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
-    DOP = fill(nut[7],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
-    POC = fill(nut[8],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
-    PON = fill(nut[9],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
-    POP = fill(nut[10],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz)
+    total_size = (g.Nx+g.Hx*2, g.Ny+g.Hy*2, g.Nz+g.Hz*2)
+    DIC = fill(nut[1],total_size) .* rand(Uniform(0.8,1.2), total_size)
+    NH4 = fill(nut[2],total_size) .* rand(Uniform(0.8,1.2), total_size)
+    NO3 = fill(nut[3],total_size) .* rand(Uniform(0.8,1.2), total_size)
+    PO4 = fill(nut[4],total_size) .* rand(Uniform(0.8,1.2), total_size)
+    DOC = fill(nut[5],total_size) .* rand(Uniform(0.8,1.2), total_size)
+    DON = fill(nut[6],total_size) .* rand(Uniform(0.8,1.2), total_size)
+    DOP = fill(nut[7],total_size) .* rand(Uniform(0.8,1.2), total_size)
+    POC = fill(nut[8],total_size) .* rand(Uniform(0.8,1.2), total_size)
+    PON = fill(nut[9],total_size) .* rand(Uniform(0.8,1.2), total_size)
+    POP = fill(nut[10],total_size) .* rand(Uniform(0.8,1.2), total_size)
 
     nutrients = nutrient_fields(DIC, NH4, NO3, PO4, DOC, DON, DOP, POC, PON, POP)
     return nutrients
 end
 function setup_nutrients(::GPUs,g,nut)
-    DIC = fill(nut[1],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
-    NH4 = fill(nut[2],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
-    NO3 = fill(nut[3],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
-    PO4 = fill(nut[4],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
-    DOC = fill(nut[5],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
-    DON = fill(nut[6],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
-    DOP = fill(nut[7],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
-    POC = fill(nut[8],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
-    PON = fill(nut[9],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
-    POP = fill(nut[10],(g.Nx, g.Ny, g.Nz)) .* rand(Uniform(0.8,1.2), g.Nx, g.Ny, g.Nz) |> CuArray
+    total_size = (g.Nx+g.Hx*2, g.Ny+g.Hy*2, g.Nz+g.Hz*2)
+    DIC = fill(nut[1],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
+    NH4 = fill(nut[2],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
+    NO3 = fill(nut[3],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
+    PO4 = fill(nut[4],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
+    DOC = fill(nut[5],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
+    DON = fill(nut[6],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
+    DOP = fill(nut[7],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
+    POC = fill(nut[8],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
+    PON = fill(nut[9],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
+    POP = fill(nut[10],total_size) .* rand(Uniform(0.8,1.2), total_size) |> CuArray
 
     nutrients = nutrient_fields(DIC, NH4, NO3, PO4, DOC, DON, DOP, POC, PON, POP)
     return nutrients
@@ -190,7 +194,28 @@ end
     load_nut_initials(paths,g)
 Load nutrient initial conditions from files
 """
-function load_nut_initials(paths,g)
+function load_nut_initials(::CPUs,paths,g)
+    total_size = (g.Nx+g.Hx*2, g.Ny+g.Hy*2, g.Nz+g.Hz*2)
+    indices = ["DIC", "NH4", "NO3", "PO4", "DOC", "DON", "DOP", "POC", "PON", "POP"]
+    pathkeys = collect(keys(paths))
+    tmps = []
+    for index in indices
+        if length(findall(x->x==index, pathkeys)) == 0
+            print("NUT_INIT: nutrient not found \n")
+        else
+            tmp = deserialize(paths[index])
+            if size(tmp) == total_size
+                push!(tmps,tmp)
+            else
+                print("NUT_INIT: grid mismatch \n")
+            end
+        end
+    end
+    nut = nutrient_fields(tmps[1],tmps[2],tmps[3],tmps[4],tmps[5],tmps[6],tmps[7],tmps[8],tmps[9],tmps[10])
+    return nut
+end
+function load_nut_initials(::GPUs,paths,g)
+    total_size = (g.Nx+g.Hx*2, g.Ny+g.Hy*2, g.Nz+g.Hz*2)
     indices = ["DIC", "NH4", "NO3", "PO4", "DOC", "DON", "DOP", "POC", "PON", "POP"]
     pathkeys = collect(keys(paths))
     tmps = []
@@ -199,7 +224,7 @@ function load_nut_initials(paths,g)
             print("NUT_INIT: nutrient not found \n")
         else
             tmp = deserialize(paths[index]) |> CuArray
-            if size(tmp) == (g.Nx, g.Ny, g.Nz)
+            if size(tmp) == total_size
                 push!(tmps,tmp)
             else
                 print("NUT_INIT: grid mismatch \n")
