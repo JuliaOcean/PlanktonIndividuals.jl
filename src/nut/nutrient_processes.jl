0c65b545d7725003d99d15863ac52a6ba9bc9023
zhenwu0728 <zhenwu@mit.edu>
2020-09-01 22:17:49 -0400

    bug fix & typo fix

--- src/nut/nutrient_processes.jl
+++ src/nut/nutrient_processes.jl
@@ -10,11 +10,12 @@ function nut_update_interior(model, velᵇ, consume, ΔT)
 
     # compute biogeochemical forcings of nutrients,for each time step
     F = nutrients_init(arch, g)
-    nut_forcing!(F, arch, g, nutrients, params, ΔT)
+    # nut_forcing!(F, arch, g, nutrients, params, ΔT)
 
     # compute nutrient diffusion,for each time step
     diffu = nutrients_init(arch, g)
     nut_diffusion!(diffu, arch, g, nutrients, params["κh"], params["κh"], params["κv"], ΔT)
+    add_nut_tendency!(diffu, F)
 
     # compute advection tendency
     nutₜ = nutrients_init(arch, g)
@@ -22,16 +23,17 @@ function nut_update_interior(model, velᵇ, consume, ΔT)
 
     # compute advection tendency for record
     gtr = nutrients_init(arch, g)
-    gtr = sub_nut_tendency!(gtr, nutₜ,nutrients)
+    sub_nut_tendency!(gtr, nutₜ,nutrients)
+    add_nut_tendency!(gtr, diffu)
 
     # apply diffusion and forcing tendency
     for name in nut_names
-        nutₜ[name].data .= nutₜ[name].data .+ diffu[name].data .+ F[name].data .+ consume[name].data ./ g.V
+        nutₜ[name].data .= nutₜ[name].data .+ diffu[name].data .+ consume[name].data ./ g.V
     end
 
     fill_halo!(nutₜ, g)
 
-    return nutₜ, add_nut_tendency!(gtr, add_nut_tendency!(diffu, F))
+    return nutₜ, gtr
 end
 
 function nut_update_interior(model, consume, ΔT)
