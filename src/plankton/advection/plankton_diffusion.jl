5feb2084fb8c96af8039684a02ef23e77e8755cf
zhenwu0728 <zhenwu@mit.edu>
2020-10-18 22:04:20 -0400

    clean up commented code

--- src/plankton/advection/plankton_diffusion.jl
+++ src/plankton/advection/plankton_diffusion.jl
@@ -1,22 +1,4 @@
 ##### calculate diffusivities of each individual
-# @kernel function calc_diffu_kernel!(plank, rnd, κx, κy, κz, ΔT)
-#     i = @index(Global, Linear)
-#     if plank[i,58] == 1.0
-#         @inbounds plank[i,1] = plank[i,1] + rnd[i,1] * √(3*ΔT) * 2*√(2*κx)
-#         @inbounds plank[i,2] = plank[i,2] + rnd[i,2] * √(3*ΔT) * 2*√(2*κx)
-#         @inbounds plank[i,3] = plank[i,3] + rnd[i,3] * √(3*ΔT) * 2*√(2*κx)
-#     end
-# end
-
-# function plankton_diffusion!(plank, rnd, arch::Architecture, κx, κy, κz, ΔT)
-#     kernel! = calc_diffu_kernel!(device(arch), 256, (size(plank,1),))
-#     event = kernel!(plank, rnd, κx, κy, κz, ΔT)
-#     wait(device(arch), event)
-#     return nothing
-# end
-
-# plankton_diffusion!(plank, rnd, arch, κ, ΔT) = plankton_diffusion!(plank, rnd, arch, κ, κ, κ, ΔT)
-
 function plankton_diffusion!(plank, rnd, κx, κy, κz, ΔT)
     plank.x .= plank.x .+ rnd.x .* √(3*ΔT) .* (2*√(2*κx)) .* plank.ac
     plank.y .= plank.y .+ rnd.y .* √(3*ΔT) .* (2*√(2*κy)) .* plank.ac
