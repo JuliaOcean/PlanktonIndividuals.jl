936116491a2fee2f239e9ce23b6cb108fc87aa15
zhenwu0728 <zhenwu@mit.edu>
2020-12-29 21:12:45 -0500

    reorganize cell division & grazing

--- src/plankton/physiology/plankton_update.jl
+++ src/plankton/physiology/plankton_update.jl
@@ -1,79 +1,43 @@
 ##### update physiological attributes of each individual
-function plankton_update!(plank, nuts, proc, rnd, par, pop,
-                          temp, nut, g::Grids, p, ΔT, t)
-    ##### calculate the total active plankton numbers
-    ##### find nutrient, temperature, and par values for each individual
-    find_NPT!(nuts, Int.(plank.xi), Int.(plank.yi), Int.(plank.zi), plank.ac, g,
-              nut.NH4.data, nut.NO3.data, nut.PO4.data, nut.DOC.data,
-              par, temp, pop, p.α, p.Φ, p.TempAe, p.Tempref, p.TempCoeff)
-
-    ##### Carbon uptake
-    calc_PS!(plank, proc, nuts, p.PCmax, p.PC_b)
-
-    ##### Nitrogen uptake
-    calc_VN!(plank, proc, nuts, g, ΔT, p.Nqmax, p.Nqmin,
-             p.VNH4max, p.VNO3max, p.VN_b, p.KsatNH4, p.KsatNO3, p.R_NC)
-
-    ##### Phosphorus uptake
-    calc_VP!(plank, proc, nuts, g, ΔT, p.Pqmax, p.Pqmin, p.VPO4max, p.VP_b, p.KsatPO4, p.R_PC)
-
-    ##### Chla
-    calc_ρchl!(plank, proc, nuts, p.Chl2N)
-
-    ##### respiration
-    calc_respir!(plank, proc, nuts, p.respir_a, p.respir_b)
+function plankton_update!(plank, nuts, proc, rnd, p, ΔT, t, arch::Architecture)
+    ##### inorganic nutrient uptake
+    calc_inorganic_uptake!(plank, proc, nuts, p, arch)
 
     ##### update C, N, P quotas
-    update1_quotas!(plank, proc, ΔT)
+    update_quotas_1!(plank, proc, ΔT, arch)
 
     ##### Organic carbon uptake if necessary
-    calc_VDOC!(plank, proc, nuts, g, ΔT, p.Cqmax, p.Cqmin, p.VDOCmax, p.VDOC_b, p.KsatDOC)
+    calc_organic_uptake!(plank, proc, nuts, p, arch)
+
+    ##### Chla
+    calc_ρchl!(plank, proc, nuts, p.Chl2N, arch)
+
+    ##### respiration
+    calc_respir!(plank, proc, nuts, p, arch)
 
     ##### update C, N, P quotas
-    update2_quotas!(plank, proc, p.R_NC, p.R_PC, ΔT)
+    update_quotas_2!(plank, proc, ΔT, p, arch)
 
     ##### Biosynthesis
-    calc_BS!(plank, proc, p.k_mtb, p.k_mtb_b, p.R_NC, p.R_PC)
-    update_biomass!(plank, proc, p.R_NC, p.R_PC, p.Cquota, p.Nsuper, ΔT)
+    calc_BS!(plank, proc, p, arch)
+    update_biomass!(plank, proc, p, ΔT, arch)
 
     ##### probabilities of grazing, mortality, and cell division
-    ##### check the probabilities every 10 mins
+    ##### check the probabilities every 5 mins
     if t%300 == 1
+        gen_rand!(rnd, arch)
         ##### grazing
-        if p.grz_P == 0
-            @inbounds plank.graz .= 0.0
-        else
-            if p.grz_stp == 0
-                calc_graz_quadratic!(nuts, plank, p.grz_P)
-            else
-                calc_graz_linear!(plank, p.grz_P, p.grz_stp)
-            end
-        end
+        calc_graz_quadratic!(plank, proc, nuts, p.grz_P, arch)
 
         ##### mortality
-        calc_mort!(plank, p.mort_reg,  p.mort_P)
+        calc_mort!(plank, proc, p, arch)
 
         ##### cell division
-        if p.dvid_type == 1
-            calc_dvid_size!(plank, p.dvid_stp, p.dvid_P, p.dvid_reg, p.Cquota, p.Nsuper)
-        elseif p.dvid_type == 2
-            calc_dvid_add!(plank, p.dvid_stp, p.dvid_P, p.dvid_reg, p.Cquota, p.Nsuper)
-        elseif p.dvid_type == 3
-            calc_dvid_age!(plank, p.dvid_stp, p.dvid_P, p.dvid_reg, p.Cquota, p.Nsuper)
-        elseif p.dvid_type == 4
-            calc_dvid_time!(plank, p.dvid_stp, p.dvid_P, p.dvid_reg, p.Cquota, p.Nsuper, t)
-        elseif p.dvid_type == 5
-            calc_dvid_ts!(plank, p.dvid_type, p.dvid_stp, p.dvid_stp2, p.dvid_P,
-                            p.dvid_reg, p.dvid_reg2, p.Cquota, p.Nsuper, t)
-        else
-            throw(ArgumentError("Wrong cell division type, must be in 1 to 5"))
-        end
+        calc_dvid!(plank, proc, divide_type(p.dvid_type), p, t, arch)
+        get_probability!(plank, proc, rnd)
     else
         plank.graz .= 0.0
         plank.mort .= 0.0
         plank.dvid .= 0.0
     end
-
-    get_rands!(plank, rnd)
 end
-
