7e9b4d4dd92eabf43370d9503679ff4a086ad51a
zhenwu0728 <zhenwu@mit.edu>
2021-03-01 12:52:20 -0500

    vertical diffusion disabled for now

--- src/diffusivity/plankton_diffusion.jl
+++ src/diffusivity/plankton_diffusion.jl
@@ -3,7 +3,7 @@
     i = @index(Global)
     @inbounds plank.x[i] = plank.x[i] + rnd.x[i] * √(1*κx*ΔT) * plank.ac[i]
     @inbounds plank.y[i] = plank.y[i] + rnd.y[i] * √(1*κy*ΔT) * plank.ac[i]
-    @inbounds plank.z[i] = plank.z[i] + rnd.z[i] * √(1*κz*ΔT) * plank.ac[i]
+    # @inbounds plank.z[i] = plank.z[i] + rnd.z[i] * √(1*κz*ΔT) * plank.ac[i]
 end
 function calc_diffusion!(plank, rnd, κx, κy, κz, ΔT, arch::Architecture)
     kernel! = calc_diffusion_kernel!(device(arch), 256, (size(plank.ac,1)))
@@ -14,7 +14,7 @@ function calc_diffusion!(plank, rnd, κx, κy, κz, ΔT, arch::Architecture)
 end
 
 function plankton_diffusion!(plank, rnd, κx, κy, κz, ΔT, g::Grids, arch::Architecture)
-    ##### generate random numbers for grazing, mortality and division
+    ##### generate random numbers for grazing, mortality and division (0,1)
     rand!(rng_type(arch), rnd.x)
     rand!(rng_type(arch), rnd.y)
     rand!(rng_type(arch), rnd.z)
