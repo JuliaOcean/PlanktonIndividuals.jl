c4335203a0a8ff38cbef34a697050a0ab405fc2b
zhenwu0728 <zhenwu@mit.edu>
2021-03-30 22:49:43 -0400

    use normal distribution instead of uniform for particle diffusion

--- src/diffusivity/plankton_diffusion.jl
+++ src/diffusivity/plankton_diffusion.jl
@@ -15,14 +15,14 @@ end
 
 function plankton_diffusion!(plank, rnd, κx, κy, κz, ΔT, g::RegularRectilinearGrid, arch::Architecture)
     ##### generate random numbers for grazing, mortality and division (0,1)
-    rand!(rng_type(arch), rnd.x)
-    rand!(rng_type(arch), rnd.y)
-    rand!(rng_type(arch), rnd.z)
+    randn!(rng_type(arch), rnd.x)
+    randn!(rng_type(arch), rnd.y)
+    # randn!(rng_type(arch), rnd.z)
 
     ##### shift the random numbers to (-1,1)
-    @inbounds rnd.x .= rnd.x .* 2.0 .- 1.0
-    @inbounds rnd.y .= rnd.y .* 2.0 .- 1.0
-    @inbounds rnd.z .= rnd.z .* 2.0 .- 1.0
+    @inbounds rnd.x .= rnd.x 
+    @inbounds rnd.y .= rnd.y 
+    # @inbounds rnd.z .= rnd.z .* 0.3 
 
     ##### calculate diffusion
     calc_diffusion!(plank, rnd, κx, κy, κz, ΔT, arch)
