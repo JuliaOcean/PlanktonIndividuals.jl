1f6454fbee64f289fb6a0c8f31daf4a195ada69b
zhenwu0728 <zhenwu@mit.edu>
2021-04-14 19:56:29 -0400

    remove @hascuda macro

--- src/architecture.jl
+++ src/architecture.jl
@@ -4,15 +4,15 @@ struct CPU <: Architecture end
 
 struct GPU <: Architecture end
 
-macro hascuda(expr)
-    return has_cuda() ? :($(esc(expr))) : :(nothing)
-end
-
 device(::CPU) = KernelAbstractions.CPU()
 device(::GPU) = KernelAbstractions.CUDADevice()
 
 array_type(::CPU) = Array
-@hascuda array_type(::GPU) = CuArray
+if CUDA.has_cuda()
+    array_type(::GPU) = CuArray
+end
 
 rng_type(::CPU) = MersenneTwister()
-@hascuda rng_type(::GPU) = CURAND.default_rng()
+if CUDA.has_cuda()
+    rng_type(::GPU) = CURAND.default_rng()
+end
