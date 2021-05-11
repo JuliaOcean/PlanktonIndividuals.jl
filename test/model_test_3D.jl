0a733cdf8f970df3434732afb70e797dd4d2d6f9
zhenwu0728 <zhenwu@mit.edu>
2021-05-11 15:45:43 -0400

    test boundary conditions

--- test/model_test_3D.jl
+++ test/model_test_3D.jl
@@ -20,6 +20,12 @@ for i in 1:11
     wvel[:,:,:,i] .= randn(16,16,17) .* 1e-4
 end
 
+# add boundary conditions for DOC
+model.nutrients.DOC.bc.x.left  = 1.0e-3 # west boundary condition of 0.1 mmol/m^2/second
+model.nutrients.DOC.bc.x.right = randn(20,20) .* 1e-3 # east boundary condition of -0.1 mmol/m^2/second
+model.nutrients.DOC.bc.y.left  = randn(20,20,11) .* 1e-3 # south boundary condition of 0.1 mmol/m^2/second
+model.nutrients.DOC.bc.y.right = randn(20,20,11) .* 1e-3 # north boundary condition of -0.1 mmol/m^2/second
+
 sim = PI_simulation(model, ΔT = 60, nΔT = 10, diag_freq = 3600, 
                     vels=(u=uvel, v=vvel, w=wvel)) 
 
