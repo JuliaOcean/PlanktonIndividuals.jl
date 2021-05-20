39b2d5c28b5c494fd554068e50e39ff8dd63a04a
zhenwu0728 <zhenwu@mit.edu>
2021-05-19 23:52:51 -0400

    reorganize func `generate_nutrients`

--- src/biogeochemistry/nutrient_fields.jl
+++ src/biogeochemistry/nutrient_fields.jl
@@ -26,46 +26,27 @@ Keyword Arguments
             or a `Dict` containing the file paths pointing to the files of nutrient initial conditions.
 - `mask` (optional): Mask out the tracers generated out of the domain, a 3D array with size `(Nx, Ny, Nz)`.
 """
-function generate_nutrients(arch, g, source::NamedTuple; mask=nothing)
+function generate_nutrients(arch, g, source::Union{Dict,NamedTuple}; mask=nothing)
     total_size = (g.Nx+g.Hx*2, g.Ny+g.Hy*2, g.Nz+g.Hz*2)
     nut = nutrients_init(arch, g)
-
-    for name in nut_names
-        nut[name].data .= fill(source[name],total_size) .* rand(0.8:1e-4:1.2, total_size) |> array_type(arch)
-        if mask ≠ nothing
-            if size(mask) == (g.Nx, g.Ny, g.Nz)
-                @views @. nut[name].data[g.Hx+1:g.Hx+g.Nx, g.Hy+1:g.Hy+g.Ny, g.Hz+1:g.Hz+g.Nz] *= mask
-            else
-                throw(ArgumentError("nut_mask: grid mismatch, size(mask) must equal to (grid.Nx, grid.Ny, grid.Nz)."))
-            end
-        end
-    end
-
-    fill_halo_nut!(nut,g)
-
-    return nut
-end
-
-function generate_nutrients(arch, g, source::Dict; mask=nothing)
-    total_size = (g.Nx+g.Hx*2, g.Ny+g.Hy*2, g.Nz+g.Hz*2)
-
-    nut = nutrients_init(arch, g)
-
     pathkeys = collect(keys(source))
-
-    tmps = []
 
     for name in nut_names
         if length(findall(x->x==name, pathkeys)) == 0
             throw(ArgumentError("NUT_INIT: nutrient not found $(name)"))
         else
-            tmp = deserialize(source[name]) |> array_type(arch)
-            if size(tmp) == (g.Nx, g.Ny, g.Nz)
-                @views @. nut[name].data[g.Hx+1:g.Hx+g.Nx, g.Hy+1:g.Hy+g.Ny, g.Hz+1:g.Hz+g.Nz] = tmp[:,:,:]
-            else
-                throw(ArgumentError("NUT_INIT:  grid mismatch"))
+            if typeof(source) <: Dict # file paths
+                tmp = deserialize(source[name]) |> array_type(arch)
+                if size(tmp) == (g.Nx, g.Ny, g.Nz)
+                    @views @. nut[name].data[g.Hx+1:g.Hx+g.Nx, g.Hy+1:g.Hy+g.Ny, g.Hz+1:g.Hz+g.Nz] = tmp[:,:,:]
+                else
+                    throw(ArgumentError("NUT_INIT:  grid mismatch"))
+                end
+            elseif typeof(source) <: NamedTuple # value
+                nut[name].data .= fill(source[name],total_size) .* rand(0.8:1e-4:1.2, total_size) |> array_type(arch)
             end
         end
+
         if mask ≠ nothing
             if size(mask) == (g.Nx, g.Ny, g.Nz)
                 @views @. nut[name].data[g.Hx+1:g.Hx+g.Nx, g.Hy+1:g.Hy+g.Ny, g.Hz+1:g.Hz+g.Nz] *= mask
@@ -76,5 +57,6 @@ function generate_nutrients(arch, g, source::Dict; mask=nothing)
     end
 
     fill_halo_nut!(nut,g)
+
     return nut
 end
