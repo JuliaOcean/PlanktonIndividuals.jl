cb75477601291bf35bce1f6e569489aa925af833
Zhen Wu <zhenwu@mit.edu>
2025-09-19 14:29:24 +0800

    update particle diffusion

--- src/Individuals/Advection/particle_diffusion.jl
+++ src/Individuals/Advection/particle_diffusion.jl
@@ -13,15 +13,10 @@ function calc_diffusion!(particle, rnd, κx, κy, κz, ΔT, arch::Architecture)
 end
 
 function particle_diffusion!(particle, rnd, κx, κy, κz, ΔT, g::AbstractGrid, arch::Architecture)
-    ##### generate random numbers for grazing, mortality and division (0,1)
+    ##### generate random numbers Normal(0,1)
     randn!(rng_type(arch), rnd.x)
     randn!(rng_type(arch), rnd.y)
-    # randn!(rng_type(arch), rnd.z)
-
-    ##### shift the random numbers to (-1,1)
-    @inbounds rnd.x .= rnd.x 
-    @inbounds rnd.y .= rnd.y 
-    # @inbounds rnd.z .= rnd.z .* 0.3 
+    randn!(rng_type(arch), rnd.z)
 
     ##### calculate diffusion
     calc_diffusion!(particle, rnd, κx, κy, κz, ΔT, arch)
@@ -29,5 +24,3 @@ function particle_diffusion!(particle, rnd, κx, κy, κz, ΔT, g::AbstractGrid,
     ##### keep individuals in the domain
     particle_boundaries!(particle, particle.ac, g, arch)
 end
-
-particle_diffusion!(particle, rnd, κ, ΔT, g, arch) = particle_diffusion!(particle, rnd, κ, κ, κ, ΔT, g, arch)
