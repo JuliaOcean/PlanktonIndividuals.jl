d4f942ef2545d4d3798705a38d1f2d20fba6a8ce
Zhen Wu <zhenwu@mit.edu>
2025-09-19 14:56:15 +0800

    fix particle diffusion

--- src/Individuals/Advection/particle_diffusion.jl
+++ src/Individuals/Advection/particle_diffusion.jl
@@ -1,13 +1,16 @@
 ##### calculate diffusivities of each individual
-@kernel function calc_diffusion_kernel!(particle, rnd, κx, κy, κz, ΔT)
+@kernel function calc_diffusion_kernel!(particle, rnd, κx, κy, κz, ΔT, g::AbstractGrid)
     i = @index(Global)
-    @inbounds particle.x[i] = particle.x[i] + rnd.x[i] * √(1*κx*ΔT) * particle.ac[i]
-    @inbounds particle.y[i] = particle.y[i] + rnd.y[i] * √(1*κy*ΔT) * particle.ac[i]
-    @inbounds particle.z[i] = particle.z[i] + rnd.z[i] * √(1*κz*ΔT) * particle.ac[i]
+    @inbounds particle.x[i] = particle.x[i] + rnd.x[i] * √(1*κx*ΔT) * particle.ac[i] / 
+                                              ΔxC(particle.xi, particle.yi, particle.zi, g)
+    @inbounds particle.y[i] = particle.y[i] + rnd.y[i] * √(1*κy*ΔT) * particle.ac[i] / 
+                                              ΔyC(particle.xi, particle.yi, particle.zi, g)
+    @inbounds particle.z[i] = particle.z[i] + rnd.z[i] * √(1*κz*ΔT) * particle.ac[i] / 
+                                              ΔzC(particle.xi, particle.yi, particle.zi, g)
 end
-function calc_diffusion!(particle, rnd, κx, κy, κz, ΔT, arch::Architecture)
+function calc_diffusion!(particle, rnd, κx, κy, κz, ΔT, g::AbstractGrid, arch::Architecture)
     kernel! = calc_diffusion_kernel!(device(arch), 256, (size(particle.ac,1)))
-    kernel!(particle, rnd, κx, κy, κz, ΔT)
+    kernel!(particle, rnd, κx, κy, κz, ΔT, g)
 
     return nothing
 end
